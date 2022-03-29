8727211048ff53db1ad85920c613719afe9de024
RBAGES <bagesremy@gmail.com>
2022-03-29 17:26:29 +0200

    rechercher des élèves

--- src/Controller/EleveController.php
+++ src/Controller/EleveController.php
@@ -10,6 +10,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Form\ClasseType;
 use App\Form\EleveType;
+use App\Form\RechercheType;
 
 class EleveController extends AbstractController
 {
@@ -27,25 +28,27 @@ class EleveController extends AbstractController
     /**
      * @Route("/eleves/ajouter", name="app_eleve_ajouter")
      */
-    public function ajouter(EleveRepository $er, Request $request): Response{
+    public function ajouter(EleveRepository $er, Request $request): Response
+    {
         $eleve = new Eleve;
         $form = $this->createForm(EleveType::class, $eleve)
-                    ->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid()){
+            ->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
             $er->add($eleve);
             return $this->redirectToRoute('app_eleve');
         }
 
-        return $this->render('formulaires/ajout.html.twig',[
-            'form'=>$form->createView(),
-            'titreform'=>'Ajouter un élève'
+        return $this->render('formulaires/ajout.html.twig', [
+            'form' => $form->createView(),
+            'titreform' => 'Ajouter un élève'
         ]);
     }
 
     /**
      * @Route("/eleve/{id}/supprimer", name="app_eleve_supprimer")
      */
-    public function supprimer($id, EleveRepository $er): Response{
+    public function supprimer($id, EleveRepository $er): Response
+    {
         $eleve = $er->find($id);
         $er->remove($eleve);
         return $this->redirectToRoute('app_eleve');
@@ -54,11 +57,50 @@ class EleveController extends AbstractController
     /**
      * @Route("/eleve/{id}/details", name="app_eleve_details")
      */
-    public function details($id,EleveRepository $er) : Response {
+    public function details($id, EleveRepository $er): Response
+    {
         $eleve = $er->find($id);
 
-        return $this->render('eleve/details.html.twig',[
+        return $this->render('eleve/details.html.twig', [
             'eleve' => $eleve,
+        ]);
+    }
+
+    /**
+     * @Route("/recherche-eleve", name="app_eleve_recherche")
+     */
+    public function rechercher(Request $request, EleveRepository $er)
+    {
+        $form = $this->createForm(RechercheType::class)->handleRequest($request);
+        $results = [];
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $data = explode(' ', $form->getData()['keywords']);
+            foreach ($data as $val) {
+                $val = '%'.$val.'%';
+                $resultNom = $er->createQueryBuilder('e')
+                            ->where('e.nom LIKE :val')
+                            ->setParameter('val',$val)
+                            ->getQuery()
+                            ->execute()
+                            ;
+                $resultPrenom = $er->createQueryBuilder('e')
+                            ->where('e.prenom LIKE :val')
+                            ->setParameter('val',$val)
+                            ->getQuery()
+                            ->execute()
+                            ;
+
+                $results = array_merge($results, $resultNom, $resultPrenom);
+            }
+            $results = array_unique($results);
+        }
+
+        return $this->render('formulaires/recherche.html.twig', [
+            'form' => $form->createView(),
+            'titreform' => 'Rechercher des élèves',
+            'resultats' => $results
         ]);
     }
 }
---
+++ src/Form/RechercheType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class RechercheType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('keywords', TextType::class, [
+                'label' => 'Mots clés'
+            ])
+            ->add('submit', SubmitType::class, [
+                'label'=>'Rechercher'
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            // Configure your form options here
+        ]);
+    }
+}
